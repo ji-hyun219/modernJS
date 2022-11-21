d8bbda3e7af1ca4410ed3cb1e8acc0a468d5059d
ji-hyun219 <ix219@hanmail.net>
2022-11-21 21:28:43 +0900

    fix(RegExp): 문자열의 앞뒤 공백 제거

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,26 @@ import "./moduleAliases";
 
 export {};
 
-const target = " Hi";
+const target = "    Hi";
+const target2 = "Hello     ";
 
 // 하나 이상의 공백으로 시작하는지 검사
 // \s 는 여러가지 공백문자 (스페이스, 탭 등)를 의미한다
 
-const regExp = /^[\s]+/;
+const regExp = /^\s+/; // + 를 사용하면 앞선 패턴을 한 번 이상 반복
 
-console.log(regExp.test(target));
+if (regExp.test(target)) {
+  const result = target.replace(regExp, "");
+  console.log(result);
+}
+
+const lastRegExp = /\s+$/;
+
+if (lastRegExp.test(target2)) {
+  const result = target2.replace(lastRegExp, "");
+  console.log(result + "end");
+}
+
+// 위 둘을 합쳐서 써보기
+const str1 = " 문자열의 앞뒤 공백 제거하기    ";
+console.log(str1.replace(/^\s+|\s+$/gm, "") + "끝");
