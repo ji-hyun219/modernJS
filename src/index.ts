c0be598f2ea7f46117e886c3b3b53721b83bca7f
ji-hyun219 <ix219@hanmail.net>
2022-11-21 20:46:55 +0900

    feat(RegExp): 시작 위치와 마지막 위치 검색

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,20 @@ import "./moduleAliases";
 
 export {};
 
-const target = "IS";
-const regexp = /is/i; // 플래그 i 는 대소문자를 구별하지 않고 검색한다
+const target = "Is this all there is?";
+const regexp = /is/g; // 문자열 내 모든 패턴을 검색하는 g 플래그
 
-if (regexp.test(target)) {
-  console.log("True");
-}
+console.log(target.match(regexp)); // 대소문자를 구분해서 길이 2인 배열이 나옴
+
+// 시작 위치로 검색
+const jihyunRepo = "https://github.com/ji-hyun219?tab=repositories";
+
+// https 로 시작하는지 검사
+const regExp = /^https/;
+
+console.log(regExp.test(jihyunRepo));
+
+// 마지막 위치로 검색
+const lastRegExp = /repositories$/;
+
+console.log(lastRegExp.test(jihyunRepo));
